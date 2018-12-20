724775bb3b86c411ca890307a475f54619439d55
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2018-12-20 11:12:35 +0600

    Added project page

--- server.js
+++ server.js
@@ -44,6 +44,13 @@ app.get('/about', (req, res) => {
         pageTitle: 'About Page'
     });
 });
+
+app.get('/project', (req, res) => {
+    res.render('project.hbs', {
+        pageTitle: 'Project Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to load the page'
